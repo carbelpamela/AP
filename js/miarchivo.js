4b5bf9bd4e7de8a1930a84582b11d59aa866dc53
Pamela Carbel <psc_138@hotmail.com>
2021-05-11 19:51:40 -0300

    adaptado

---
+++ js/miarchivo.js
@@ -0,0 +1,7 @@
+$('.vermas').prepend('<h6 id="title">¿Por qué pintar mates?. <br><br> Elegimos "el mate" por la importancia que tiene este objeto en nuestra cultura, y los significados que nos vienen a la mente cuando pensamos en tomarnos unos ricos mates; amargos, dulces, con yuyo, con frutas y tantos otros. Pensamos en mate, pensamos en compartir, en la unión con otros, en las charlas , pero también como consuelo, como compañero en las noches largas de estudio, para celebrar la vida o acortar las distancias cuando nos toco dejar nuestro suelo tan amado. </br> </br>  Nuestro anhelo es llegar a todo el país y por qué no también al resto del mundo. Por ahora viajamos en nuestra combi, y vamos dejando un pedacito de nosotros en cada lugar y en cada persona combinando nuestro arte y nuestras ganas de vivir una nueva vida. Creando lo que creemos.</h6> <br><button class="btn  btn-warning" id="mostrarTitulo">Leer más..</button>');
+$('#mostrarTitulo').on('click', function(){
+    $('#title').toggle(600); 
+})
+
+
+
